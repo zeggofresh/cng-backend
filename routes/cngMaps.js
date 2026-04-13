04007844d9223d96374fb7d8bf54db2ca3752d9a
zeggofresh <zeggofresh@gmail.com>
2026-04-13 14:33:15 +0530

    gf

--- routes/cngMaps.js
+++ routes/cngMaps.js
@@ -49,11 +49,11 @@ router.get('/nearby', [
         )) AS distance_km
       FROM cng_pumps
       WHERE is_active = true
-      HAVING (6371 * acos(
-        cos(radians($1)) * cos(radians(latitude)) * 
-        cos(radians(longitude) - radians($2)) + 
-        sin(radians($1)) * sin(radians(latitude))
-      )) <= $3
+        AND (6371 * acos(
+          cos(radians($1)) * cos(radians(latitude)) * 
+          cos(radians(longitude) - radians($2)) + 
+          sin(radians($1)) * sin(radians(latitude))
+        )) <= $3
       ORDER BY distance_km ASC
     `;
 
